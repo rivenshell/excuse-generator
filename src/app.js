d3c4abe60667e7c78926e6f2649f5cddb384df2a
Riv <rivenshell111@gmail.com>
2022-09-24 00:59:35 +0000

    Working Prototype2

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The man under my bed", "My computer", "The Dishwasher", "My Cat"];
-let action = ["ate", "broke", "lost", "swallowed"];
-let what = ["my shoes", "my keys", "the broken vase", "my essay"];
+let who = [
+  " The man under my bed",
+  " My computer",
+  " The Dishwasher",
+  " My Cat"
+];
+let action = ["ate", "broke", " lost", "swallowed"];
+let what = [" my shoes", " my keys", " the broken vase", " my essay"];
 let when = [
   "last night",
   "when I was sleeping",
@@ -24,6 +29,13 @@ window.onload = function() {
 
   // = values[Math.floor]
 
-  document.getElementById("excuse").innerHTML = pullexcuse(who);
+  let word1 = pullexcuse(who);
+  let word2 = pullexcuse(action);
+  let word3 = pullexcuse(what);
+  let word4 = pullexcuse(when);
+
+  let result = word1 + word2 + word3 + word4;
+  document.getElementById("excuse").innerHTML = result;
+  // document.getElementById("excuse").innerHTML = pullexcuse(what);
   console.log();
 };
