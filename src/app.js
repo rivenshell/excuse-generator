8909cc7d7a4f8e479026328053397f22c0c8ef54
Riv <rivenshell111@gmail.com>
2022-09-24 00:25:54 +0000

    Working Prototype

--- src/app.js
+++ src/app.js
@@ -5,17 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The man under my bed", "My computer", "The Dishwasher", "My Cat"];
+let action = ["ate", "broke", "lost", "swallowed"];
+let what = ["my shoes", "my keys", "the broken vase", "my essay"];
+let when = [
+  "last night",
+  "when I was sleeping",
+  "during my shower",
+  "on the way here"
+];
+
+function pullexcuse(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 window.onload = function() {
   //write your code here
-  let who = ["The man under my bed", "My computer", "The Dishwasher", "My Cat"];
-  let action = ["ate", "broke", "lost", "swallowed"];
-  let what = ["my shoes", "my keys", "the broken vase", "my essay"];
-  let when = [
-    "last night",
-    "when I was sleeping",
-    "during my shower",
-    "on the way here"
-  ];
 
-  console.log(onload());
+  // = values[Math.floor]
+
+  document.getElementById("excuse").innerHTML = pullexcuse(who);
+  console.log();
 };
